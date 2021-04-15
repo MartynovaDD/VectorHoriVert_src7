d60543a5721bcbe7136803c6d670487e0de479b9
Darya Martynova <daria.mart@bk.ru>
2021-04-15 12:02:31 +0300

    2

--- python.py
+++ python.py
@@ -12,12 +12,20 @@ def func(InputFile1,InputFile2,OutputFile):
     except FileNotFoundError:
         print("Error! Cannot find file...")
         exit(-1)
-    fg_w, fg_h = foreground.size
-    bg_w, bg_h = background.size
-    offset = ((bg_w - fg_w) // 2, (bg_h - fg_h) // 2)
-    background.paste(foreground, offset)
-    background.save(OutputFile)
-    
+    arr1=array(background)
+    arr2=array(foreground)
+    if arr1.shape[0]<arr2.shape[0] or arr1.shape[1]<arr2.shape[1]:
+        print("Размер неверный")
+        exit()
+    k = int((arr1.shape[0]-arr2.shape[0])/2)
+    m = int((arr1.shape[1]-arr2.shape[1])/2)
+    for i in range(k,k+arr2.shape[0]):
+        for j in range(m,m+arr2.shape[1]):
+            arr1[i,j,0]=arr2[i-k,j-m,0]
+            arr1[i,j,1]=arr2[i-k,j-m,1]
+            arr1[i,j,2]=arr2[i-k,j-m,2]
+    img = Image.fromarray(arr1)
+    img.save(OutputFile)
 arg1 = input()
 arg2 = input()
 arg3 = input()
